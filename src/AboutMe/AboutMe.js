6552958b9712879c77766fad1d0e1e2cc0bfa01d
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-28 15:47:35 -0500

    home icons added

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -3,7 +3,11 @@ import React from 'react'
 const AboutMe = () => {
   return (
     <div id="aboutme">
-      
+      <div>
+        <p>Kailey Lee</p>
+        <p>Full stack web developer</p>
+        <p>Kailey Lee is a junior software engineer specializing in Ruby on Rails and Javascript along with React and Redux. </p>
+      </div>
     </div>
   )
 }
--- src/Home/HomeText.js
+++ src/Home/HomeText.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeText = () => {
   return (
-    <div>
-      <div className="mainColorBox"></div>
+    <div id="homeText">
+
       <div className="mainTitle">
         <p>{`{ 'Junior Developer + logical thinker'`}</p>
         <p className="mainTitleLine2">: ' <span className="mainTitleName">&nbsp;KAILEY LEE&nbsp;</span> ' }</p>
       </div>
+
+      <div className="mainIcons">
+        <a href="/aboutme">
+          <i class="fas fa-info-circle" />
+        </a>
+        <a target="_blank" href="https://github.com/kailey16">
+          <i class="fab fa-github" />
+        </a>
+        <a target="blank" href="https://www.linkedin.com/in/kaileyleemcintosh/">
+          <i class="fab fa-linkedin" />
+        </a>
+      </div>
+
     </div>
   )
 }
