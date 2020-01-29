06e60a836138afdbf462fd972d9cf1cf190edbf6
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-28 22:22:09 -0500

    aboutme icons added:

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -5,16 +5,27 @@ const AboutMe = () => {
   return (
     <div id="aboutme">
       <div className="bio">
-        Welcome to visit my website!<br/>
-        My name is <span className="bioName">Kailey Lee</span> and I am a <span className="bioStatus">Full Stack Web Developer</span>
+        Hi, my name is <span className="bioName">Kailey Lee</span> and I am a <span className="bioStatus">Full Stack Web Developer</span>
         
         <p className="bioDetail">
           Kailey Lee is a junior software engineer specializing in Ruby on Rails and Javascript along with React and Redux. Her educational background is in Chemical Engineering and Business(MBA). She has experience working in Retail Operation Management focusing on client satisfaction and business partner relations coupled with event planning and management. She wanted to change her career to software engineering because she truly loves problem solving and developing logical thought process.
         </p>
         
         {`{'Technical Skills': [ Ruby on Rails, Javascript, React.js, Redux.js, HTML5, CSS/Bootstrap/Semantic UI, SQL/SQLite3/PostgreSQL, RESTful architecture, MVC pattern, RSpec, Git, APIs ]`}
-
       </div>
+
+      <div className="aboutmeIcons">
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/kailey16">
+          <i class="fab fa-github aboutmeIcon" />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kaileyleemcintosh/">
+          <i class="fab fa-linkedin aboutmeIcon" />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@klee.mcintosh">
+          <i class="fab fa-medium aboutmeIcon"></i>
+        </a>
+      </div>
+
     </div>
   )
 }
--- src/Home/HomeText.js
+++ src/Home/HomeText.js
@@ -13,10 +13,10 @@ const HomeText = () => {
         <a href="/aboutme">
           <i class="fas fa-info-circle mainIcon" />
         </a>
-        <a target="_blank" href="https://github.com/kailey16">
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/kailey16">
           <i class="fab fa-github mainIcon" />
         </a>
-        <a target="blank" href="https://www.linkedin.com/in/kaileyleemcintosh/">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kaileyleemcintosh/">
           <i class="fab fa-linkedin mainIcon" />
         </a>
       </div>
