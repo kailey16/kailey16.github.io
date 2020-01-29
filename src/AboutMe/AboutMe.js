77cc21c63217983762c2f4df521f8de1dee45319
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-29 11:58:18 -0500

    bio paragraphs are done

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import '../style/AboutMe.css'
 
+
+// resume https://drive.google.com/file/d/1SbjTS29VEL5foaeoAJDQ4nFTkxRkUs5z/view?usp=sharing
+
 const AboutMe = () => {
   return (
     <div id="aboutme">
       <div className="bio">
-        Hi, my name is <span className="bioName">Kailey Lee</span> and I am a <span className="bioStatus">Full Stack Web Developer</span>
-        
-        <p className="bioDetail">
-          Kailey Lee is a junior software engineer specializing in Ruby on Rails and Javascript along with React and Redux. Her educational background is in Chemical Engineering and Business(MBA). She has experience working in Retail Operation Management focusing on client satisfaction and business partner relations coupled with event planning and management. She wanted to change her career to software engineering because she truly loves problem solving and developing logical thought process.
+        <p className="bioLine bioName">
+          {`{ ' Kailey Lee ' : ' Full Stack Web Developer ' ,`}
         </p>
-        
-        {`{'Technical Skills': [ Ruby on Rails, Javascript, React.js, Redux.js, HTML5, CSS/Bootstrap/Semantic UI, SQL/SQLite3/PostgreSQL, RESTful architecture, MVC pattern, RSpec, Git, APIs ]`}
+
+        <p className="bioLine bioDetail">
+          {`' Who is she? ' : ' Kailey Lee is a junior software engineer specializing in Ruby on Rails and Javascript along with React and Redux. Her educational background is in Chemical Engineering and Business(MBA). She has experience working in Retail Operation Management focusing on client satisfaction and business partner relations coupled with event planning and management. She wanted to change her career to software engineering because she truly loves problem solving and developing logical thought process.', `}
+        </p>
+        <p className="bioLine bioDetail">
+          {`' Technical Skills ': [ Ruby on Rails, Javascript, React.js, Redux.js, HTML5, CSS/Bootstrap/Semantic UI, SQL/SQLite3/PostgreSQL, RESTful architecture, MVC pattern, RSpec, Git, APIs ]}`}
+        </p>
       </div>
 
       <div className="aboutmeIcons">
