838c8d792c88243e6f404ebc1c90261bf46b39c6
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-29 13:23:34 -0500

    aboutme page done

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -19,7 +19,7 @@ const AboutMe = () => {
         <p className="bioLine bioDetail">
           <span className="colorYellow bioStatus">Background?</span>&nbsp; : &nbsp;Her educational background is in Chemical Engineering and Business(MBA). She has experience working in Retail Operation Management focusing on client satisfaction and business partner relations coupled with event planning and management. She truly loves problem solving and developing logical thought process, so she decided to change her career to software engineering ,
         </p>
-        <p className="bioLine bioDetail">
+        <p className="bioLine skillDetail">
           <span className="colorYellow bioStatus">Technical Skills</span>&nbsp; : &nbsp;[ &nbsp;&nbsp;<span className="colorYellow">Ruby on Rails</span> , <span className="colorYellow">Javascript</span> , <span className="colorYellow">React.js</span> , <span className="colorYellow">Redux.js</span> ,<br/>
           <span className="colorYellow skillLine">HTML5</span> , <span className="colorYellow">CSS/Bootstrap/Semantic UI</span> , <span className="colorYellow">SQL/SQLite3/PostgreSQL</span> ,<br/>
           <span className="colorYellow skillLine">RESTful architecture</span> , <span className="colorYellow">MVC pattern</span> , <span className="colorYellow">RSpec</span> , <span className="colorYellow">Git</span> , <span className="colorYellow">APIs</span>&nbsp; ]<br/>
@@ -31,10 +31,10 @@ const AboutMe = () => {
       <div className="aboutmeIcons">
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/kailey16">
           <i className="fab fa-github aboutmeIcon" />
-        </a>
+        </a><br/>
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kaileyleemcintosh/">
           <i className="fab fa-linkedin aboutmeIcon" />
-        </a>
+        </a><br/>
         <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@klee.mcintosh">
           <i className="fab fa-medium aboutmeIcon"></i>
         </a>
