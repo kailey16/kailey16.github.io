79351c2d7d17020544b757c7df691a8d81dee30d
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-31 18:00:37 -0500

    preview styling added

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div id="navbar" className="nav justify-content-center">
-      <Link className="navOption nav-link" to="/">HOME</Link>
-      <Link className="navOption nav-link" to="/aboutme">ABOUT</Link>
-      <Link className="navOption nav-link" to="/project">PROJECT</Link>
-      <Link className="navOption nav-link" to="/blog">BLOG</Link>
+      <Link className="navOption nav-link" to="/" style={{color: "#660033"}}>HOME</Link>
+      <Link className="navOption nav-link" to="/aboutme" style={{color: "#660033"}}>ABOUT</Link>
+      <Link className="navOption nav-link" to="/project" style={{color: "#660033"}}>PROJECT</Link>
+      <Link className="navOption nav-link" to="/blog" style={{color: "#660033"}}>BLOG</Link>
     </div>
   )
 }
--- src/Project/Preview.js
+++ src/Project/Preview.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Preview = (props) => {
   return (
     <div className="preview">
+      <p className="previewTitle">{props.title}</p>
       <img className="previewImg" src={props.src} alt="previewImg" />
-      <p>{props.title}</p>
     </div>
   )
 }
