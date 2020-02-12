0961fb09458e1c7141c596b9bcceb323eda1f724
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-12 13:18:14 -0500

    floof finder page done

--- src/Project/FloofFinder.js
+++ src/Project/FloofFinder.js
@@ -1,16 +1,15 @@
 import React from 'react'
-// import FFdemo from '../media/FloofFinder/FFdemo.mp4'
+import FFdemo from '../media/FloofFinder/FFdemo.mp4'
 
 const FloofFinder = () => {
   return(
     <div className="projectShow">
       <div className="projectTitle">FloofFinder</div>
       <div className="projectDetail">
-      FloofFinder
+        Floof Finder is a single page web application to find shelter dogs for adoption and collect their information
       </div>
 
-      {/* <video className="demoVideo" controls src={FFdemo} /> */}
-
+      {/* Image Carousel */}
       <div id="carouselExampleIndicators" class="carousel slide projectCarousel" data-ride="carousel">
         <ol class="carousel-indicators">
           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -32,6 +31,44 @@ const FloofFinder = () => {
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
+      </div>
+
+
+      <div className="frontbackDetail">
+        <div>
+          <div className="endTitle">Backend
+            <span>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/kailey16/mod3-project-flooffinder">
+              <i className="fab fa-github showIcon" />
+              </a>
+            </span>
+          </div>
+
+          <i className="fas fa-caret-right arrowIcon"/> Ruby on Rails<br/>
+          <i className="fas fa-caret-right arrowIcon"/> SQLite3<br/>
+          <i className="fas fa-caret-right arrowIcon"/> RESTful architecture + MVC pattern + Serialized JSON data<br/>
+          <i className="fas fa-caret-right arrowIcon"/> Transported real-time data from PetFinder API<br/>
+          <i className="fas fa-caret-right arrowIcon"/> Implemented sessions with Sessions controller
+        </div>
+        <div>
+          <div className="endTitle">Frontend
+            <span>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/kailey16/mod3-project-flooffinder">
+              <i className="fab fa-github showIcon" />
+              </a>
+            </span>
+          </div>
+
+          <i className="fas fa-caret-right arrowIcon"/> Javascript<br/>
+          <i className="fas fa-caret-right arrowIcon"/> Custom CSS + Bootstrap framework<br/>
+          <i className="fas fa-caret-right arrowIcon"/> Drag and drop function to save the pet under my list
+        </div>
+      </div>
+
+
+      <div className="demo">
+          <div className="demoTitle">DEMO Video</div>
+          <video className="demoVideo" controls src={FFdemo} />
       </div>
     </div>
   )
