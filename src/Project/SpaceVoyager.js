17d13b940c47a76f48899f6c82aab2400bf18a2b
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-11 20:08:13 -0500

    SV detail page started

--- src/Project/SpaceVoyager.js
+++ src/Project/SpaceVoyager.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../style/SV.css'
+import SVdemo from '../media/SpaceVoyager/SVdemo.mp4'
 
 const SpaceVoyager = () => {
   return(
     <div id="SpaceVoyager">
-      SpaceVoyager
+      <div id="SVtitle">SpaceVoyager</div>
+      <video id='SVdemo' controls src={SVdemo} />
     </div>
   )
 }
