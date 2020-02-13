6e4ce6800cc899b6d225b87b9ffd34403ddc9128
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-13 15:31:43 -0500

    aboutme page responsive done

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -7,18 +7,16 @@ const AboutMe = () => {
       <div className="bio">
         <p className="bioName">{`{`}</p>
         <p className="bioLine bioName">
-          <span className="colorYellow">Kailey Lee</span> &nbsp; : &nbsp;Full Stack Web Developer ,
+          <span className="color1">Kailey Lee</span> &nbsp; : &nbsp;Full Stack Web Developer ,
         </p>
         <p className="bioLine bioDetail">
-          <span className="colorYellow bioStatus">Who is she?</span> &nbsp; : &nbsp;Kailey Lee is a junior software engineer specializing in Ruby on Rails and Javascript accompanying React and Redux ,
+          <span className="color1 bioStatus">Who is she?</span> &nbsp; : &nbsp;Kailey Lee is a junior software engineer specializing in Ruby on Rails and Javascript accompanying React and Redux ,
         </p>
         <p className="bioLine bioDetail">
-          <span className="colorYellow bioStatus">Background?</span>&nbsp; : &nbsp;Her educational background is in Chemical Engineering and Business(MBA). She has experience working in Retail Operation Management focusing on client satisfaction and business partner relations coupled with event planning / management. She truly loves problem solving and developing logical thought process, so she decided to change her career to software engineering ,
+          <span className="color1 bioStatus">Background?</span>&nbsp; : &nbsp;Her educational background is in Chemical Engineering and Business(MBA). She has experience working in Retail Operation Management focusing on client satisfaction and business partner relations coupled with event planning / management. She truly loves problem solving and developing logical thought process, so she decided to change her career to software engineering ,
         </p>
         <p className="bioLine skillDetail">
-          <span className="colorYellow bioStatus">Technical Skills</span>&nbsp; : &nbsp;[ &nbsp;&nbsp;<span className="colorYellow">Ruby on Rails</span> , <span className="colorYellow">Javascript</span> , <span className="colorYellow">React.js</span> , <span className="colorYellow">Redux.js</span> ,<br/>
-          <span className="colorYellow skillLine">HTML5</span> , <span className="colorYellow">CSS/Bootstrap/Semantic UI</span> , <span className="colorYellow">SQL/SQLite3/PostgreSQL</span> ,<br/>
-          <span className="colorYellow skillLine">RESTful architecture</span> , <span className="colorYellow">MVC pattern</span> , <span className="colorYellow">RSpec</span> , <span className="colorYellow">Git</span> , <span className="colorYellow">APIs</span>&nbsp; ]<br/>
+          <span className="color1 bioStatus">Technical Skills</span>&nbsp; : &nbsp;[ &nbsp;&nbsp;<span className="color2">Ruby on Rails</span> , <span className="color2">Javascript</span> , <span className="color2">React.js</span> , <span className="color2">Redux.js</span> , <span className="color2 skillLine">HTML5</span> , <span className="color2">CSS/Bootstrap/Semantic UI</span> , <span className="color2">SQL/SQLite3/PostgreSQL</span> , <span className="color2 skillLine">RESTful architecture</span> , <span className="color2">MVC pattern</span> , <span className="color2">RSpec</span> , <span className="color2">Git</span> , <span className="color2">APIs</span>&nbsp; ]
         </p>
         <p className="bioName">{`}`}</p>
       </div>
@@ -26,15 +24,15 @@ const AboutMe = () => {
 
       <div className="aboutmeIcons">
         <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1SbjTS29VEL5foaeoAJDQ4nFTkxRkUs5z/view?usp=sharing">
-          <i className="far fa-file-pdf aboutmeIcon" /><br/>
+          <i className="far fa-file-pdf aboutmeIcon" />
           <p className="resume">RESUME</p>
         </a>
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/kailey16">
           <i className="fab fa-github aboutmeIcon" />
-        </a><br/>
+        </a>
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kaileyleemcintosh/">
           <i className="fab fa-linkedin aboutmeIcon" />
-        </a><br/>
+        </a>
         <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@klee.mcintosh">
           <i className="fab fa-medium aboutmeIcon"></i>
         </a>
